e26853f446504c81d80406bd6fefe7e8538ee7b3
raphael <Raphael.morin2009@gmail.com>
2025-10-26 17:23:46 +0100

    Refactor menu function to update Temperature instead of powerPercentage

--- Serial.ino
+++ Serial.ino
@@ -3,39 +3,39 @@ void menu() {
     int readchar = Serial.read();
     switch (readchar) {
       case '1':
-        powerPercentage += 5;
+        Temperature += 5;
         break;
       case '2':
-        powerPercentage += 1;
+        Temperature += 1;
         break;
       case '3':
-        powerPercentage += 0.5;
+        Temperature += 0.5;
         break;
       case '4':
-        powerPercentage += 0.1;
+        Temperature += 0.1;
         break;
       case '5':
-        powerPercentage += 0.01;
+        Temperature += 0.01;
         break;
       case '6':
-        powerPercentage -= 0.01;
+        Temperature -= 0.01;
         break;
       case '7':
-        powerPercentage -= 0.1;
+        Temperature -= 0.1;
         break;
       case '8':
-        powerPercentage -= 0.5;
+        Temperature -= 0.5;
         break;
       case '9':
-        powerPercentage -= 1;
+        Temperature -= 1;
         break;
       case '0':
-        powerPercentage -= 5;
+        Temperature -= 5;
         break;
       default: break;
     }
-    Serial.print("powerPercentage :");
-    Serial.println(powerPercentage);
+    Serial.print("Temperature :");
+    Serial.println(Temperature);
     Serial.println("1 +5");
     Serial.println("2 +1");
     Serial.println("3 +0.5");
@@ -47,12 +47,10 @@ void menu() {
     Serial.println("9 -1");
     Serial.println("0 -5");
     Serial.println();
-    if (powerPercentage > 100) {
-      powerPercentage = 100;
+    if (Temperature > 100) {
+      Temperature = 100;
     }
-    if (powerPercentage < 0) {
-      powerPercentage = 0;
-    }
-    wait_time = powerPercentage_to_wait(powerPercentage);
+    if (Temperature < 0) {
+      Temperature = 0;
   }
 }
