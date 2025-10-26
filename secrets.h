3f149e2118272ef5b85fc270c28a084fa7c65982
raphael <Raphael.morin2009@gmail.com>
2025-10-26 13:15:40 +0100

    Refactor comments in secrets.h for clarity and consistency

--- secrets.h
+++ secrets.h
@@ -1,3 +1,4 @@
-#define Secret_ssid "your ssid"; //enter ssid
+// WIFI and API key secrets
+#define Secret_ssid "your ssid"; //enter your network's SSID
 #define Secret_password "your password"; //enter your network's password
-#define api_key "your key";  // create api key
+#define api_key "your key";  //create your API key
