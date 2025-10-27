635703e0a132534346eff5d88a83c40d6cdda4f6
raphael <Raphael.morin2009@gmail.com>
2025-10-27 12:02:07 +0100

    Implement menu function for temperature adjustment and add WiFi handling with API key validation

---
+++ Solartriac/secrets.h
@@ -0,0 +1,4 @@
+// WIFI and API key secrets
+#define Secret_ssid "your network's SSID"     //enter your network's SSID
+#define Secret_password "your network's password"  //enter your network's password
+#define api_key "your API key"          //create your API key
--- secrets.h
+++
@@ -1,4 +0,0 @@
-// WIFI and API key secrets
-#define Secret_ssid "your ssid" //enter your network's SSID
-#define Secret_password "your password" //enter your network's password
-#define api_key "your key"  //create your API key
